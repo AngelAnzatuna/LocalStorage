bb68d1beaedd1e6db11a9d42ee6a7f46514e250c
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-02 11:10:13 +0100

    Implementar validación y manejo de errores al agregar un tweet

--- js/app.js
+++ js/app.js
@@ -13,5 +13,29 @@ function eventos() {
 function agregarTweet(e) {
     e.preventDefault();
 
-    console.log('Agregando Tweet...');
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validación
+    if(tweet === '') {
+        mostrarError('Un mensaje no puede ir vacio')
+        return; // Evita que se ejecute más lineas de código
+    }
+    console.log('agregando tweet');
+}
+
+// Mostrar Mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el Contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta después de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
