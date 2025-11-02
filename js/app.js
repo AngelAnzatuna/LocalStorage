04648dfc49f039e8b166e23535c93c7f07f0f925
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-02 10:59:16 +0100

    Seleccionando elementos a trabajar, crear el evento y empezando a crear su funcionalidad

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets =[];
+
+// Eventos
+eventos()
+function eventos() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+
+    console.log('Agregando Tweet...');
+}
